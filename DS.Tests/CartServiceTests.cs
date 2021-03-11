6c2e532e42f13756457e2ed5d93ae2491d183613
Kirill Elizarov <kiril.elizarov@gmail.com>
2021-03-11 23:54:05 +0100

    add ICartService and straightforward implementation add unit test project and simple unit test

---
+++ DS.Tests/CartServiceTests.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Models;
+using NUnit.Framework;
+
+namespace DS.Tests
+{
+	[TestFixture]
+	public class CartServiceTests
+	{
+		private ICartService _service;
+		[SetUp]
+		public void Setup()
+		{
+			// TODO: IOC injections
+			_service = new CartService();
+		}
+
+		[Test, TestCaseSource(nameof(AddInputNearestCommonNode))]
+		public void Test1(IEnumerable<Item> items, double total)
+		{
+			foreach (var item in items)
+			{
+				_service.Add(item);
+			}
+
+			_service.GetTotal().Should().Be(total);
+		}
+		
+		// TestCaseData
+		private static IEnumerable<TestCaseData> AddInputNearestCommonNode()
+		{
+			var x = new List<Item>();
+			x.Add(new Item(3));
+			x.Add(new Item(4));
+			x.Add(new Item(1));
+			yield return new TestCaseData(x, 8);
+		}
+	}
+	
+}
---
+++ Models/ICartService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Models
+{
+    public interface ICartService
+    {
+        void Add(Item item);
+        void Remove(Item item);
+        double GetTotal();
+    }
+
+    public class CartService : ICartService
+    {
+        private double _counter = 0; 
+        public void Add(Item item)
+        {
+            _counter += item.Price;
+        }
+
+        public void Remove(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTotal()
+        {
+            return _counter;
+        }
+    }
+
+    public class Item
+    {
+        public readonly double Price;
+        public Item(double price)
+        {
+            Price = price;
+        }
+
+    }
+}
