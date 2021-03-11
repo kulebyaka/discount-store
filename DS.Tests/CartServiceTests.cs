e3f4fb69175b8a80bfdf41c46413818f3f83a571
Kirill Elizarov <kiril.elizarov@gmail.com>
2021-03-12 00:20:06 +0100

    add Ioc container

--- DS.Tests/CartServiceTests.cs
+++ DS.Tests/CartServiceTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using DryIoc;
 using FluentAssertions;
 using Models;
 using NUnit.Framework;
@@ -8,24 +9,26 @@ namespace DS.Tests
 	[TestFixture]
 	public class CartServiceTests
 	{
-		private ICartService _service;
+		private IContainer _container;
 		
 		[SetUp]
 		public void Setup()
 		{
-			// TODO: IOC injections
-			_service = new CartService();
+			_container = new Container();
+			_container.Register<ICartService, CartService>();
 		}
 
 		[Test, TestCaseSource(nameof(AddInput))]
 		public void GetTotalTest(IEnumerable<Item> items, double total)
 		{
+			var service = _container.Resolve<ICartService>();
+			
 			foreach (var item in items)
 			{
-				_service.Add(item);
+				service.Add(item);
 			}
 
-			_service.GetTotal().Should().Be(total);
+			service.GetTotal().Should().Be(total);
 		}
 		
 		// TestCaseData
