982b245d17b33d81f7f5c8f3e1e0f0835e7647f8
Kirill Elizarov <kiril.elizarov@gmail.com>
2021-03-14 17:21:47 +0100

    add promotion "3 for 0.90€"

---
+++ Models/DiscountRules/PromoutionXForYRule.cs
@@ -0,0 +1,23 @@
+﻿using Models.Repositories;
+
+namespace Models.DiscountRules
+{
+	public class PromoutionXForYRule : IDiscountRule
+	{
+		private readonly int _specialCostQuantity;
+		private readonly decimal _specialCost;
+
+		public PromoutionXForYRule(int specialCostQuantity, decimal specialCost)
+		{
+			this._specialCostQuantity = specialCostQuantity;
+			this._specialCost = specialCost;
+		}
+
+		public decimal Apply(ICartItem cartItem)
+		{
+			int quantity = cartItem.Quantity;
+			return (quantity / _specialCostQuantity)*_specialCost 
+			       + (quantity%_specialCostQuantity)*cartItem.Product.Price;
+		}
+	}
+}
